d31033d11692f59efa08b7966a579da5194de3f7
Shashi Kiran Reddy <shashikiranr78@gmail.com>
2020-08-30 14:11:11 +0530

    Update breast_cancer.py

--- breast_cancer.py
+++ breast_cancer.py
@@ -1,16 +1,20 @@
+# Import necessary packages and libraries
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier 
 import matplotlib.pyplot as plt
 breast_cancer_data = load_breast_cancer()
+# print data to terminal to view it
 print(breast_cancer_data)
 print(breast_cancer_data.data[0])
 print(breast_cancer_data.feature_names)
 print(breast_cancer_data.target)
 print(breast_cancer_data.target_names)
+# Splitting data for training and testing
 training_data, validation_data, training_labels, validation_labels = train_test_split(breast_cancer_data.data,breast_cancer_data.target,test_size = 0.2,random_state = 100)
 print(len(training_data))
 print(len(training_labels))
+# finding accuracies for different values of k
 accuracies = []
 for k in range(1,101):
   classifier = KNeighborsClassifier(n_neighbors = k)
